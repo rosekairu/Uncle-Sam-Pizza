ea8f385c7c5ed9f7124681357de2938bc0c2ce68
Rose Kairu <rosekairu@gmail.com>
2020-04-18 21:10:07 +0300

    modify menu file for order calculation forms

---
+++ js/billing.js
@@ -0,0 +1,15 @@
+var deliveryaddress;
+
+function insert(myForm) {
+
+    //var a = prompt("Please enter your delivery address", "Your address here");
+    document.getElementsByClassName('panel panel-primary').value = a;
+    deliveryaddress = a;
+
+    myForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+    }
+};

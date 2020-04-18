3824befe74e2153567f0bd69041d4f303104b2a8
Rose Kairu <rosekairu@gmail.com>
2020-04-18 20:32:32 +0300

    add images to file

--- js/script.js
+++ js/script.js
@@ -21,59 +21,7 @@ function total(myForm) {
     }
     if (document.getElementById('large').checked) {
 
-        total += 7.00
-    }
-    if (document.getElementById('topping0').checked) {
-        count++;
-    }
-    if (document.getElementById('topping1').checked) {
-        count++;
-    }
-    if (document.getElementById('topping2').checked) {
-        count++;
-    }
-    if (document.getElementById('topping3').checked) {
-        count++;
-    }
-    if (document.getElementById('topping4').checked) {
-        count++;
-    }
-    if (document.getElementById('topping5').checked) {
-        count++;
-    }
-    if (document.getElementById('topping6').checked) {
-        count++;
-    }
-    if (document.getElementById('topping7').checked) {
-        count++;
-    }
-    if (document.getElementById('topping8').checked) {
-        count++;
-    }
-    if (document.getElementById('topping9').checked) {
-        count++;
-    }
-    if (document.getElementById('topping10').checked) {
-        count++;
-    }
-    if (document.getElementById('topping11').checked) {
-        count++;
-    }
-    if (document.getElementById('topping12').checked) {
-        count++;
-    }
 
-    total += count * topping;
-    if (document.getElementById('delivery').checked) {
-        total = total * 1.10;
-    }
-    var result = Math.round(total * 100) / 100;
-    if (document.getElementById('delivery').checked) {
-        alert('Your Total will be $' + result + ' delivered to ' + deliveryaddress);
-    } else {
-        alert('Your Total will be $' + result + ' and will be ready for pickup in 30 minutes');
-    }
-}
 
 function blank(myForm) {
     document.getElementById('carryout').checked = true;
@@ -83,5 +31,3 @@ function blank(myForm) {
         var topname = "topping" + a;
         document.getElementById(topname).checked = false;
     }
-
-}

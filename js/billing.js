f3bf13a7567be6b7d02b59daf401e140f23d8fce
Rose Kairu <rosekairu@gmail.com>
2020-04-20 07:51:04 +0300

    modify about file

--- js/billing.js
+++ js/billing.js
@@ -26,11 +26,3 @@ $(document).ready(function () {
 
     });
 });
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
-}
-
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
-}
